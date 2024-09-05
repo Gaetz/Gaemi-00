f28f8babad4fe10e86521d7637b48be126b511d5
Gaëtan Blaise-Cazalet <gaetan.blaisecazalet@gmail.com>
2024-09-05 17:23:52 +0200

    [feature] Function shader exercise solution

--- assets/shader03-functions/shaders/shader.fs
+++ assets/shader03-functions/shaders/shader.fs
@@ -38,6 +38,18 @@ void main() {
 // Exercise 2. Texture smoothstep.
 // Load a texture and run the colours through smoothstep before displaying the texture
 
+
+// varying vec2 vUvs;
+//
+// uniform sampler2D diffuse;
+//
+// void main(void) {
+//   vec4 diffuseSample = texture2D(diffuse, vUvs);
+//
+//   gl_FragColor = smoothstep(vec4(0.0), vec4(1.0), diffuseSample);
+// }
+
+
 // Exercise 3. min, max, clamp.
 // Change value2 to min(fragTexCoord.x, 0.25), max(fragTexCoord.x, 0.75), clamp(fragTexCoord.x, 0.25, 0.75).
 // How clamp is linked to min and max ?
